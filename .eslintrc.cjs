48c9583af042f26e180a7d8afbbd87655086f8a2
Abraham Galindo <abraham.garo@gmail.com>
2023-06-28 20:02:20 -0600

    Add eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,20 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'quotes': [ 'error', 'single' ],
+    'eol-last': [ 'error', 'always' ],
+    'no-trailing-spaces': [ 'error', { 'skipBlankLines': true }],
+    'comma-dangle': ['error', { 'objects': 'always-multiline' }],
+    'indent': [ 'error',  2 ],
+    'arrow-parens': [ 'error', 'as-needed' ],
+    'arrow-spacing': 'error',
+    'key-spacing': ['error', { 'beforeColon': false }],
   },
 }
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # react-vitest-msw
 
 Example of SPA using MSW for unit and E2E testing
+
+The project was created with the initial [guide](https://vitejs.dev/guide/)
