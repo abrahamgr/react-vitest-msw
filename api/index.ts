c8d50557ff8d95552f45f626250d9cc6bfe3627c
Abraham Galindo <abraham.garo@gmail.com>
2023-10-07 16:00:12 -0600

    Initial monorepo setup

--- api/index.ts
+++ api/index.ts
@@ -5,13 +5,24 @@ import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox'
 import { login } from './routes/login.ts'
 import { db } from './data/index.ts'
 
+const port: number = process.env.PORT ? Number(process.env.PORT) : 8080
+
+const isProduction = process.env.NODE_ENV === 'production'
 
 const fastify = Fastify({
-  logger: true,
+  logger: {
+    level: 'error',
+  },
 }).withTypeProvider<TypeBoxTypeProvider>()
 
 fastify.register(cors, {
   origin: (origin, cb) => {
+    // no origin allowed for testing only
+    if(!isProduction && !origin) {
+      cb(null, true)
+      return
+    }
+
     const hostname = new URL(origin as string).hostname
     if(hostname === 'localhost'){
     //  Request from localhost will pass
@@ -40,7 +51,7 @@ fastify.register(login, registerOptions)
 // Run the server!
 try {
   await fastify.listen({
-    port: 8080,
+    port,
   })
 } catch (err) {
   fastify.log.error(err)
